f520f309ffb25d0ad5c62cddf99a66915fda4396
mihkelal <mihkelal@gmail.com>
2018-09-10 23:32:08 +0300

    Prune deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,14 @@
-server '142.93.132.68', roles: [:web, :app, :db], primary: true
+set :application, 'do-mihkel'
+set :user, 'mihkel'
 
-set :repo_url,        'git@github.com:mihkelal/do-mihkel.git'
-set :application,     'do-mihkel'
-set :user,            'mihkel'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :linked_dirs,  %w{.bundle log public/system public/uploads tmp/cache tmp/pids tmp/sockets vendor/bundle}
 set :linked_files, %w{config/master.key}
+set :puma_init_active_record, true
+# set :puma_preload_app, true # Not sure if necessary
+set :repo_url, 'git@github.com:mihkelal/do-mihkel.git'
+
+server '142.93.132.68', user: fetch(:user), roles: [:web, :app, :db], primary: true
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -73,12 +49,8 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
+  before :starting, :check_revision
+  after :finishing, :compile_assets
+  after :finishing, :cleanup
+  after :finishing, :restart
 end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
