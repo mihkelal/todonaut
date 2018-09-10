2edba3b80568626bee45f6cfd7fa76163cd18694
mihkelal <mihkelal@gmail.com>
2018-09-10 13:51:38 +0300

    Make deploy.rb use private, not public key

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-# Change these
 server '142.93.132.68', roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:mihkelal/do-mihkel.git'
@@ -18,7 +17,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
