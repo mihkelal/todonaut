4e896e340215f8cebbdbc546438368f51e0ad091
mihkelal <mihkelal@gmail.com>
2019-02-06 11:12:27 +0200

    Rename project to Todonaut

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, 'do-mihkel'
+set :application, 'todonaut'
 set :user, 'mihkel'
 
 set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
@@ -6,5 +6,5 @@ set :linked_dirs,  %w{.bundle log public/system public/uploads tmp/cache tmp/pid
 set :linked_files, %w{config/master.key}
 set :puma_init_active_record, true
 # set :puma_preload_app, true # Not sure if necessary
-set :repo_url, 'git@github.com:mihkelal/do-mihkel.git'
+set :repo_url, 'git@github.com:mihkelal/todonaut.git'
 set :rvm_ruby_version, '2.6.0'
