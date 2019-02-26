267e103c12d1353f0ca6a4e187bfb9844cb574bd
Mihkel Alavere <mihkel@wisemedia.ee>
2019-02-26 15:52:55 +0200

    Add username or email presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, uniqueness: true
-  validates :email, uniqueness: true
+  validates :username, uniqueness: true, if: -> { username.present? }
+  validates :email, uniqueness: true, if: -> { email.present? }
   validates :password_digest, uniqueness: true, presence: true
+  validate :username_or_email_presence
+
+  private
+
+  def username_or_email_presence
+    return if username.present? || email.present?
+
+    errors.add(:base, 'Username or email must be present')
+  end
 end
