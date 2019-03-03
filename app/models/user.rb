a0e570bbd8e4ea20e5d5dc2acff32708ab514b65
Mihkel Alavere <mihkelal@gmail.com>
2019-03-03 19:59:57 +0200

    Add associations between note and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :notes, dependent: :destroy
+
   validates :username, uniqueness: true, presence: true
   validates :password_digest, uniqueness: true, presence: true
 end
