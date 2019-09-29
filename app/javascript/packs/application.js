bcb387afb611a4e0b8c81e86104ee1217ec6ae0c
mihkelal <mihkelal@gmail.com>
2019-09-29 21:41:43 +0300

    Update all npm packages (#9)
    * Replace activestorage with @rails/activestorage
    
    * Replace rails-ujs with @rails/ujs
    
    * Replace double quotes with single quotes
    
    * Redo turbolinks require
    
    * Update all packages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,16 +9,13 @@
 
 console.log('Hello World from Webpacker')
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-import Rails from 'rails-ujs';
-import Turbolinks from 'turbolinks';
-import * as ActiveStorage from 'activestorage';
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+require('@rails/ujs').start()
+require('turbolinks').start()
+require('@rails/activestorage').start()
 import 'app/app.scss';
 
 const application = Application.start()
-const context = require.context("controllers", true, /.js$/)
+const context = require.context('controllers', true, /.js$/)
 application.load(definitionsFromContext(context))
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
