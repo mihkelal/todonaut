86992515d0684888d85cf9f9ef9740a9e895151b
mihkelal <mihkelal@gmail.com>
2019-10-20 19:53:12 +0300

    Add storage directory to linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'todonaut'
 set :user, 'app'
 
 set :deploy_to, "/var/www/#{fetch(:application)}"
-set :linked_dirs, %w[.bundle log public/system public/uploads tmp/cache tmp/pids tmp/sockets vendor/bundle]
+set :linked_dirs, %w[.bundle log public/system public/uploads storage tmp/cache tmp/pids tmp/sockets vendor/bundle]
 set :linked_files, %w[config/master.key]
 set :puma_init_active_record, true
 # set :puma_preload_app, true # Not sure if necessary
