1fbe8826a36b9ec8648e722a720175f2f971c959
mihkelal <mihkelal@gmail.com>
2019-10-27 21:34:05 +0200

    Add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brand: {
+          black: '#565656',
+          blood: '#76323F',
+          grain: '#D7CEC7',
+          tan: '#C09F80',
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
