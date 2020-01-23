fa1ff3cdd88b4f635e1956f60b1229b1e60bb7a2
mihkelal <mihkel.alavere@eesti.ee>
2020-01-23 11:48:14 +0200

    Add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './app/views/**/*.slim'
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
@@ -8,6 +18,7 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
   ]
 }
