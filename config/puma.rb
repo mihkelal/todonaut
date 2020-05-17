af641f1b2c5858ad6cd4ae1b0e719d3700ab72dd
mihkelal <mihkel.alavere@eesti.ee>
2020-05-17 17:53:28 +0300

    Reset max_threads_count

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@
 # and maximum; this matches the default thread size of Active Record.
 #
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
-min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { 1 }
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
