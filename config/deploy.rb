6f730f3305becb28059b742d2bcb937a60399106
mihkelal <mihkel.alavere@eesti.ee>
2020-06-01 14:36:07 +0300

    Remove storage from linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'todonaut'
 set :user, 'app'
 
 set :deploy_to, "/var/www/#{fetch(:application)}"
-set :linked_dirs, %w[.bundle log public/system public/uploads storage tmp/cache tmp/pids tmp/sockets vendor/bundle]
+set :linked_dirs, %w[.bundle log public/system public/uploads tmp/cache tmp/pids tmp/sockets vendor/bundle]
 set :linked_files, %w[config/master.key]
 set :repo_url, 'git@github.com:mihkelal/todonaut.git'
 set :rvm_ruby_version, '2.6.2'
