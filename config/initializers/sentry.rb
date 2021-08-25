d49722120219d85b0da8fc2c6a5e9d6842053950
mihkelal <mihkel.alavere@eesti.ee>
2021-08-25 17:01:26 +0400

    Add github workflow for CI

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.dsn = Rails.application.credentials.sentry[:dsn]
+  config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.enabled_environments = %w[production]
   config.excluded_exceptions = []
   config.send_default_pii = true
