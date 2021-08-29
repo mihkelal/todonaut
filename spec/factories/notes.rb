ab3d52edded62b2f082060b744afd3a01b712551
mihkelal <mihkel.alavere@eesti.ee>
2021-08-29 17:12:11 +0400

    Add User creates note system spec

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -2,4 +2,11 @@
 
 FactoryBot.define do
   factory :note
+
+  trait :with_some_attributes do
+    title { Faker::Lorem.sentence }
+    description { [Faker::Lorem.paragraph, nil].sample }
+    started_at { [Faker::Time.backward(days: 10), nil].sample }
+    completed_at { [Time.current, nil].sample }
+  end
 end
---
+++ spec/system/registered_user_creates_note_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Registered user creates note' do
+  let(:user) { create(:user) }
+  let(:note) { attributes_for(:note, :with_some_attributes) }
+
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'with all fields filled' do
+    visit login_path
+    fill_in 'Username', with: user.username
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_text 'Successfully logged in'
+
+    visit new_note_path
+    fill_in 'Title', with: note[:title]
+    fill_in 'Description', with: note[:description]
+    fill_in 'Started at', with: note[:started_at]
+    fill_in 'Ended at', with: note[:completed_at]
+    attach_file 'Files', 'spec/fixtures/text_file.txt'
+    click_on 'Create Note'
+
+    expect(page).to have_text 'Note successfully created.'
+    expect(page).to have_text note[:title]
+  end
+end
