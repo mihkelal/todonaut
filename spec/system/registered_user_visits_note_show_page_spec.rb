1a3494a2cd9266e636e8c71f3a4b3b96f543f65c
mihkelal <mihkel.alavere@eesti.ee>
2021-08-30 16:24:54 +0400

    Add User views note show page system spec

---
+++ spec/system/registered_user_visits_note_show_page_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Registered user visits note show page' do
+  let(:user) { create(:user) }
+  let(:users_note) { create(:note, :with_some_attributes, user: user) }
+  let(:other_note) { create(:note, :with_some_attributes, user: create(:user)) }
+
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'when note blongs to user' do
+    visit login_path
+    fill_in 'Username', with: user.username
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_text 'Successfully logged in'
+
+    visit note_path(users_note)
+    expect(page).to have_text users_note.title
+  end
+
+  it 'when note does not blong to user' do
+    visit login_path
+    fill_in 'Username', with: user.username
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_text 'Successfully logged in'
+
+    visit note_path(other_note)
+    expect(page).to have_text 'You are not authorized to perform this action'
+    expect(page).to have_no_text other_note.title
+  end
+end
