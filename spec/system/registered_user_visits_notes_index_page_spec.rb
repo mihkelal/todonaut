87b8161766edb8b44d47af2e0cba3806ca9a2add
mihkelal <mihkel.alavere@eesti.ee>
2021-08-30 16:17:58 +0400

    Fix let name

--- spec/system/registered_user_visits_notes_index_page_spec.rb
+++ spec/system/registered_user_visits_notes_index_page_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Registered user visits notes index page' do
   let(:user) { create(:user) }
   let!(:users_note) { create(:note, :with_some_attributes, user: user) }
-  let!(:public_user) { create(:note, :with_some_attributes) }
+  let!(:public_note) { create(:note, :with_some_attributes) }
 
   before do
     driven_by(:rack_test)
@@ -17,7 +17,7 @@ RSpec.describe 'Registered user visits notes index page' do
     fill_in 'Password', with: user.password
     click_button 'Log in'
 
-    expect(page).to have_no_text public_user.title
+    expect(page).to have_no_text public_note.title
     expect(page).to have_text users_note.title
   end
 end
