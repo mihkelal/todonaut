388deb838ac32708f3128b29c8fe3c010bb3601c
mihkelal <mihkel.alavere@eesti.ee>
2021-09-23 11:20:50 +0300

    Change configuration to send less stuff to Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@ Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.enabled_environments = %w[production]
-  config.excluded_exceptions = []
+  config.excluded_exceptions += ['ActionController::RoutingError', 'ActiveRecord::RecordNotFound']
   config.send_default_pii = true
-  config.traces_sample_rate = 1
+  config.traces_sample_rate = 0.2
 end
