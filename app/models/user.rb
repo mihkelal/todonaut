4381b5a12a61708b55b78e2511b930ebf78a6022
mihkelal <mihkel.alavere@eesti.ee>
2021-09-24 18:11:31 +0300

    Add around_action :switch_locale

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  enum locale: I18n.available_locales.map(&:to_s).then{_1.zip(_1)}.to_h, _prefix: true
+
   has_many :notes, dependent: :destroy
 
   validates :username, uniqueness: true, presence: true
