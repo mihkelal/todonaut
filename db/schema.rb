8f6849115c6e28cdcb45d1984724b8169723cbbe
mihkelal <mihkel.alavere@eesti.ee>
2021-09-24 10:40:31 +0300

    Add AddLocaleToUsers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_162616) do
+ActiveRecord::Schema.define(version: 2021_09_17_142057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_07_23_162616) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "locale"
     t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
