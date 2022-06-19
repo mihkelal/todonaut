af467767e20029d5c28d0d9a50f3a73cdf4f85b0
mihkelal <mihkel.alavere@eesti.ee>
2022-06-19 22:51:28 +0300

    Update specs

--- spec/system/unregistered_user_completes_note_spec.rb
+++ spec/system/unregistered_user_completes_note_spec.rb
@@ -5,9 +5,9 @@ require 'rails_helper'
 RSpec.describe 'Unregistered user completes note' do
   let(:note) { create(:note, :with_some_attributes, completed_at: nil) }
 
-  it 'when note is public', :js do
+  it 'when note is public' do
     visit note_path(note)
-    click_link 'Mark as completed'
+    click_button 'Mark as completed'
 
     expect(page).to have_text 'Note successfully marked as completed'
   end
