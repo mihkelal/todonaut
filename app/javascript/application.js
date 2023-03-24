4e3707db59fa3b9fd1b61803f74a8b4105e5641d
Mihkel Alavere <mihkel.alavere@eesti.ee>
2023-03-24 17:52:21 +0200

    Add html lang attribute (#57)
    * Add html lang attribute
    
    * Rearrange js files
    
    * Update html lang on locale switch

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import * as ActiveStorage from "@rails/activestorage"
-ActiveStorage.start()
-import "trix"
-import "@rails/actiontext"
+/*
+ *= require direct_upload
+ *= require locale
+ */
