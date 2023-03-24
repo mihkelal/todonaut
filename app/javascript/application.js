e91ec534e1d4f04490231b04e7415567086b4662
mihkelal <mihkel.alavere@eesti.ee>
2023-03-24 17:39:04 +0200

    Rearrange js files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import * as ActiveStorage from "@rails/activestorage"
-ActiveStorage.start()
-import "trix"
-import "@rails/actiontext"
+/*
+ *= require direct_upload
+ */
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,7 @@
 
 # Pin npm packages by running ./bin/importmap
 
-pin 'application', preload: true
+pin 'es_modules', preload: true
 
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@rails/activestorage', to: 'activestorage.esm.js'
