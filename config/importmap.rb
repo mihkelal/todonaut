57fc05f2336fdd47ab7c4c96933cc1636b1aeb46
mihkelal <mihkelal@gmail.com>
2024-05-20 09:36:35 +0300

    Stop marking importmap preloads as it is default now

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,9 @@
 
 # Pin npm packages by running ./bin/importmap
 
-pin 'es_modules', preload: true
+pin 'es_modules'
 
-pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@rails/activestorage', to: 'activestorage.esm.js'
 pin 'trix'
 pin '@rails/actiontext', to: 'actiontext.js'
