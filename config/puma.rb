2660b737661b8847be7081264f9ec0926d009d78
mihkelal <mihkelal@gmail.com>
2024-05-20 09:43:54 +0300

    Use Puma plugin for live css rebuilding instead of foreman bin/dev

--- config/puma.rb
+++ config/puma.rb
@@ -38,3 +38,6 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+# run the Tailwind watch process in the background.
+plugin :tailwindcss if ENV.fetch("RAILS_ENV", "development") == "development"
