d953b5cd94910cbf790fe4736196563ba30e8d99
mihkelal <mihkelal@gmail.com>
2024-06-18 20:32:49 +0300

    Delete code related to pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,11 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  include Pundit::Authorization
-
   delegate :current_user, to: :helpers, allow_nil: true
-
-  rescue_from Pundit::NotAuthorizedError, with: :redirect_user_back
 
   around_action :switch_locale
 
   private
-
-  def redirect_user_back
-    redirect_back(fallback_location: root_path, alert: t('global.not_authorized'))
-  end
 
   def switch_locale(&)
     locale = helpers.current_user_locale
